5d1a4f3da99d104a955ac9e8c004e019fc5f5b59
Chisom Anuforom <grantnufor@yahoo.com>
2020-05-13 19:16:10 +0100

    updates 3

---
+++ app/src/main/java/HttpAdapter/RegKeyHttpServiceAdapter.java
@@ -0,0 +1,59 @@
+package HttpAdapter;
+
+import org.json.JSONObject;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+public class RegKeyHttpServiceAdapter {
+
+    public JSONObject GetRegKeyByRegKeyDetail(String regKeyDetail)
+    {
+//        String SOAP_ACTION_GetJsonData ="http://tempuri.org/GetUserByUserNameAndPassword";
+        String SOAP_ACTION_GetJsonData ="http://tempuri.org/GetRegKeyByRegKeyDetail";
+        String OPERATION_NAME_GetJsonData = "GetRegKeyByRegKeyDetail";
+
+        String WSDL_TARGET_NAMESPACE ="http://tempuri.org/";
+        String SOAP_ADDRESS ="http://junior.landoria.org/WebServices/RegKeyServices.asmx";
+        SoapPrimitive response = null;
+
+        JSONObject jsonObj = new JSONObject();
+
+        try
+        {
+            SoapObject request = new SoapObject(WSDL_TARGET_NAMESPACE,OPERATION_NAME_GetJsonData);
+            request.addProperty("regKeyDetail", regKeyDetail);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = true;
+            envelope.setOutputSoapObject(request);
+            HttpTransportSE httpTransport = new HttpTransportSE(SOAP_ADDRESS);
+            httpTransport.call(SOAP_ACTION_GetJsonData, envelope);
+
+            response = (SoapPrimitive) envelope.getResponse();
+            jsonObj = new JSONObject(response.toString());
+
+//            ///JSONArray jsonArray = mainJson.getJSONArrayundefined"student");
+//            if(response.toString().length()>0 )
+//            {
+//
+//            }
+//            else
+//            {
+//                //response="Empty";
+//            }
+
+        }
+        catch (Exception exception)
+        {
+            //response="error: "+exception.toString();
+            exception.printStackTrace();
+            //Toast.makeTextundefinedthis, exception.printStackTraceundefined) ,Toast.LENGTH_LONG).showundefined);
+        }
+
+        return jsonObj;
+
+    }
+
+}
